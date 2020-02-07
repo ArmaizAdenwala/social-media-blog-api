9decd5b3ac4125ef7abf0e8a4d41bdb585242a0f
Armaiz Adenwala <armaiz.adenwala@gmail.com>
2020-02-07 12:19:29 -0800

    add uuid to user

--- db/migrate/20200207201655_devise_create_users.rb
+++ db/migrate/20200207201655_devise_create_users.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
-  def change
-    create_table :users do |t|
+  def change 
+    enable_extension 'pgcrypto'
+    create_table :users, id: :uuid  do |t|
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -11,4 +12,5 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     end
 
     add_index :users, :email,                unique: true
+  end
 end
