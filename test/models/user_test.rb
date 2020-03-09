9078fb745f3335f43b00415c63958e31fea5bdf2
Armaiz Adenwala <armaiz.adenwala@gmail.com>
2020-03-09 09:17:17 -0700

    add user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user if passwords do not match" do
+    user = User.new(email: 'john@doe.com', password: 'password', password_confirmation: 'password2')
+    assert_not user.save
+  end
+
+  test "should save user if passwords match" do
+    user = User.new(email: 'john@doe.com', password: 'password', password_confirmation: 'password')
+    assert user.save
+  end
+
+  test "should not save user if duplicate email" do
+    user1 = User.new(email: 'john@doe.com', password: 'password', password_confirmation: 'password')
+    user2 = User.new(email: 'john@doe.com', password: 'password', password_confirmation: 'password')
+    assert user1.save
+    assert user2.save
+  end
 end
